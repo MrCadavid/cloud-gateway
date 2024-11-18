4e425266e50dc374a5c25da9ed5db93f3224895c
brayancadavid <bcadavid@unicesar.edu.co>
2024-11-18 15:11:04 -0500

    @feat:added documentation general to our api gateway

--- src/main/java/com/hexagon/cloud_gateway/CloudGatewayApplication.java
+++ src/main/java/com/hexagon/cloud_gateway/CloudGatewayApplication.java
@@ -3,13 +3,40 @@ package com.hexagon.cloud_gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@OpenAPIDefinition(
+        info = @Info(
+                title = "API Gateway",
+                version = "1.0",
+                description = "Documentation API Gateway v1.0"
+        )
+)
 public class CloudGatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CloudGatewayApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CloudGatewayApplication.class, args);
+    }
+
+    @Bean
+ public RouteLocator routeLocator(RouteLocatorBuilder builder) {
+  return builder
+    .routes()
+    .route(r -> r.path("/event-service/v3/api-docs").and().method(HttpMethod.GET).uri("lb://EVENTS"))
+    .route(r -> r.path("/notification-service/v3/api-docs").and().method(HttpMethod.GET).uri("lb://NOTIFICATIONS"))
+    .build();
+ }
+  
+
+
+   
 
 }
+
